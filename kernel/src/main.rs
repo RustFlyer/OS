79f8fe0af8d4d884092ce4a3026d2fff12893518
EchudeT <452439311@qq.com>
2025-03-05 15:08:20 +0800

    kernel init

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+#![no_std]
+#![no_main]
+
+mod console;
+mod lang_item;
+mod sbi;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("entry.S"));
+
+
+
+#[unsafe(no_mangle)]
+pub fn rust_main() -> ! {
     println!("Hello, world!");
+    sbi::shutdown(false);
+    loop {}
 }
