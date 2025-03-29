541c803b8d931b568a78b2a69d1620ed96904157
EchudeT <452439311@qq.com>
2025-03-29 17:17:03 +0800

    fix minor bug

--- lib/vfs/src/inode.rs
+++ lib/vfs/src/inode.rs
@@ -5,7 +5,7 @@ use config::{
     inode::{InodeMode, InodeState, InodeType},
     vfs::{Stat, TimeSpec},
 };
-use downcast_rs::{Downcast, impl_downcast};
+use downcast_rs::{Downcast, DowncastSync, impl_downcast};
 use mutex::SpinNoIrqLock;
 
 extern crate alloc;
@@ -40,7 +40,7 @@ impl InodeMeta {
     }
 }
 
-pub trait Inode: Send + Sync + Downcast {
+pub trait Inode: Send + Sync + DowncastSync {
     fn get_meta(&self) -> &InodeMeta;
 
     fn get_attr(&self) -> SysResult<Stat>;
