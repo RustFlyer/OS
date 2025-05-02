a9e2627d063f56d729db9ba128c5b0738c92a9ab
iponika <18023803967@163.com>
2025-05-02 11:17:35 +0800

    temp save

--- kernel/src/task/signal/futex.rs
+++ kernel/src/task/signal/futex.rs
@@ -1,8 +1,10 @@
+use core::ops::ControlFlow;
+
 use alloc::sync::Arc;
 use mm::address::VirtAddr;
-use systype::SysResult;
+use systype::{SysError, SysResult};
 
-use crate::{task::Task, vm::user_ptr::SumGuard};
+use crate::{processor::current_task, task::Task, vm::user_ptr::{PageFaultAccessType, SumGuard}};
 
 pub struct FutexAddr {
     pub addr: VirtAddr,
@@ -13,8 +15,8 @@ impl FutexAddr {
     pub fn raw(&self) -> usize {
         self.addr.into()
     }
-    pub fn check(&self, task: &Arc<Task>) -> SysResult<()> {
-        task.just_ensure_user_area(self.addr, size_of::<VirtAddr>(), PageFaultAccessType::RO)
+    pub fn check(&self) -> SysResult<()> {
+        current_task().just_ensure_user_area(self.addr, size_of::<VirtAddr>(), PageFaultAccessType::RO)
     }
     pub fn read(&self) -> u32 {
         unsafe { atomic_load_acquire(self.addr.0 as *const u32) }
@@ -27,5 +29,66 @@ impl From<usize> for FutexAddr {
             addr: a.into(),
             _guard: SumGuard::new(),
         }
+    }
+}
+
+impl Task {
+    fn just_ensure_user_area(
+        &self,
+        begin: VirtAddr,
+        len: usize,
+        access: PageFaultAccessType,
+    ) -> SysResult<()> {
+        self.ensure_user_area(begin, len, access, |_, _| ControlFlow::Continue(()))
+    }
+
+    /// Ensure that the whole range is accessible, or return an error.
+    fn ensure_user_area(
+        &self,
+        begin: VirtAddr,
+        len: usize,
+        access: PageFaultAccessType,
+        mut f: impl FnMut(VirtAddr, usize) -> ControlFlow<Option<SysError>>,
+    ) -> SysResult<()> {
+        if len == 0 {
+            return Ok(());
+        }
+
+        unsafe { set_kernel_user_rw_trap() };
+
+        let test_fn = match access {
+            PageFaultAccessType::RO => will_read_fail,
+            PageFaultAccessType::RW => will_write_fail,
+            _ => panic!("invalid access type"),
+        };
+
+        let mut curr_vaddr = begin;
+        let mut readable_len = 0;
+        while readable_len < len {
+            if test_fn(curr_vaddr.0) {
+                self.with_mut_memory_space(|m| m.handle_page_fault(curr_vaddr, access))?
+            }
+
+            let next_page_beg: VirtAddr = VirtAddr::from(curr_vaddr.floor().next());
+            let len = next_page_beg - curr_vaddr;
+
+            match f(curr_vaddr, len) {
+                ControlFlow::Continue(_) => {}
+                ControlFlow::Break(None) => {
+                    unsafe { set_kernel_trap() };
+                    return Ok(());
+                }
+                ControlFlow::Break(Some(e)) => {
+                    unsafe { set_kernel_trap() };
+                    return Err(e);
+                }
+            }
+
+            readable_len += len;
+            curr_vaddr = next_page_beg;
+        }
+
+        unsafe { set_kernel_trap() };
+        Ok(())
     }
 }
