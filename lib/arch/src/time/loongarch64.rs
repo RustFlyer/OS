72168f07d0f3e8e668ccbeaf7a27999487cfa320
adong660 <adong660@outlook.com>
2025-05-07 21:11:56 +0800

    Add LoongArch version of `arch::time`.

---
+++ lib/arch/src/time/loongarch64.rs
@@ -0,0 +1,45 @@
+use core::time::Duration;
+
+use loongArch64::register::tcfg;
+use loongArch64::time::{Time, get_timer_freq};
+use spin::Lazy;
+
+use config::board::INTERRUPTS_PER_SEC;
+
+// On LoongArch, we can get the timer frequency via the CPUCFG instruction.
+static FREQ: Lazy<usize> = Lazy::new(get_timer_freq);
+
+pub fn get_time() -> usize {
+    Time::read()
+}
+
+pub fn get_time_ms() -> usize {
+    Time::read() / (*FREQ / 1000)
+}
+
+pub fn get_time_us() -> usize {
+    Time::read() / (*FREQ / 1_000_000)
+}
+
+pub fn get_time_duration() -> Duration {
+    Duration::from_micros(get_time_us() as u64)
+}
+
+/// Set the next timer interrupt.
+///
+/// This function sets the next timer interrupt to occur after a specified number of
+/// clock ticks.
+pub fn set_nx_timer_irq() {
+    // This function does nothing on LoongArch, as timer interrupts can be set to be
+    // raised periodically.
+}
+
+/// Initialize the timer.
+///
+/// This function must be called once to set up the timer.
+pub fn init_timer(times: usize) {
+    let ticks = (times * *FREQ / INTERRUPTS_PER_SEC + 3) & !3;
+    tcfg::set_periodic(true);
+    tcfg::set_init_val(ticks);
+    tcfg::set_en(true);
+}
---
+++ lib/arch/src/time/riscv64.rs
@@ -0,0 +1,40 @@
+use core::time::Duration;
+
+use riscv::register::time;
+
+use config::board::{CLOCK_FREQ, INTERRUPTS_PER_SEC};
+
+pub fn get_time() -> usize {
+    time::read()
+}
+
+pub fn get_time_ms() -> usize {
+    time::read() / (CLOCK_FREQ / 1000)
+}
+
+pub fn get_time_us() -> usize {
+    time::read() / (CLOCK_FREQ / 1_000_000)
+}
+
+pub fn get_time_duration() -> Duration {
+    Duration::from_micros(get_time_us() as u64)
+}
+
+/// Set the next timer interrupt.
+///
+/// This function sets the next timer interrupt to occur after a specified number of
+/// clock ticks.
+pub fn set_nx_timer_irq() {
+    let next_trigger: u64 = (time::read() + CLOCK_FREQ / INTERRUPTS_PER_SEC)
+        .try_into()
+        .unwrap();
+    sbi_rt::set_timer(next_trigger);
+}
+
+/// Initialize the timer.
+///
+/// This function must be called once to set up the timer.
+pub fn init_timer(times: usize) {
+    // This function does nothing on RISC-V, as there is nothing to do before
+    // calls to `set_nx_timer_irq`.
+}
