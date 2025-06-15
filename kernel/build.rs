5188b3ed8b79b8c5fd9680cdf5b42a05a0785877
adong660 <adong660@outlook.com>
2025-06-16 01:31:50 +0800

    Small changes.

--- kernel/build.rs
+++ kernel/build.rs
@@ -7,33 +7,30 @@ fn main() {
 
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
-
-    let link_script = fs::read_to_string(PathBuf::from(manifest_dir).join("link.ld")).unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_triple = env::var("TARGET").unwrap();
+    let compile_profile = env::var("PROFILE").unwrap();
 
     let ram_size = RAM_SIZE - KERNEL_RAM_OFFSET;
-
-    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
 
     // Note: Remember to copy the constants from `lib/config/src/mm.rs` to here.
     // This is a workaround for the fact that the build script is compiled for the host
     // architecture, not the target architecture of the kernel.
     let VIRT_START: usize = match target_arch.as_str() {
-        "riscv64" => {
-            0xffff_ffc0_8000_0000
-        }
-        "loongarch64" => {
-            0x9000_0000_8000_0000
-        }
+        "riscv64" => 0xffff_ffc0_8000_0000,
+        "loongarch64" => 0x9000_0000_8000_0000,
         _ => panic!("Unsupported target architecture"),
     };
     let KERNEL_START: usize = VIRT_START + KERNEL_RAM_OFFSET;
 
-    let new = link_script
+    // Generate the linker script.
+    let link_script = fs::read_to_string(PathBuf::from(manifest_dir).join("linker.ld"))
+        .unwrap()
         .replace("%RAM_START%", &KERNEL_START_PHYS.to_string())
         .replace("%VIRT_START%", &KERNEL_START.to_string())
         .replace("%RAM_SIZE%", &ram_size.to_string());
 
-    let dest = PathBuf::from(out_dir).join("link.ld");
-    fs::write(&dest, new).unwrap();
-    println!("cargo:rustc-link-arg=-T{}", dest.display());
+    let linker_script_dest = PathBuf::from(out_dir).join("linker.ld");
+    fs::write(&linker_script_dest, link_script).unwrap();
+    println!("cargo:rustc-link-arg=-T{}", linker_script_dest.display());
 }
